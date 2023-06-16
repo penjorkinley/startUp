e6cda69f5a8e5f931e5f752e7e371cd188827ed5
Jigme Tenzin Chogyel <jigmetenzinchogyel@gmail.com>
2023-06-16 15:39:43 +0600

    cool push

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Layout from "./Layouts/Layout"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomeLayout from "./Layouts/HomeLayout"
+import HomePage from "./pages/HomePage"
+import DashboardLayout from "./Layouts/DashboardLayout"
+import DashboardPage from "./pages/DashboardPage"
 
 function App() {
+
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomeLayout /> } />
+          <Route path="/" element={<HomeLayout />}>
+            <Route index element={<HomePage />}/>
+            <Route path="dashboard" element={<DashboardLayout />}>
+              <Route index element={<DashboardPage />}/>
+            </Route>
           </Route>
         </Routes>
       </Router>
