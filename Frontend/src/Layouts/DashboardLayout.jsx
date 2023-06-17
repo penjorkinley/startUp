dc392d1d90c3cd717df6f6a1a33b674da4558f25
PrernaChhetri <110452388+PrernaChhetri@users.noreply.github.com>
2023-06-17 11:30:24 +0600

    dashboard

--- Frontend/src/Layouts/DashboardLayout.jsx
+++ Frontend/src/Layouts/DashboardLayout.jsx
@@ -3,13 +3,11 @@ import DashHeader from "../components/DashHeader";
 
 function DashboardLayout() {
   return (
-    <div className="flex">
-      <div className="w-1/4 bg-gray-200">
-        {/* Sidebar or Dashboard content */}
+    <div className="relative min-h-screen">
+      <div className="fixed top-0 left-0 h-full bg-gray-200 w-1/4 flex-shrink-0">
         <DashHeader />
       </div>
-      <div className="w-3/4 bg-white p-4">
-        {/* Main content */}
+      <div className="absolute top-0 right-0 bottom-0 left-1/4 bg-white p-4">
         <Outlet />
       </div>
     </div>
