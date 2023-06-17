fd1eccb56b6e40e4f5d0e4d8f9a1c5ab9abf52df
Jigme Tenzin Chogyel <jigmetenzinchogyel@gmail.com>
2023-06-17 10:09:07 +0600

    fri

--- Frontend/src/Auth/Signin.jsx
+++ Frontend/src/Auth/Signin.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-<<<<<<< HEAD
 import logo from "../assets/logo.png";
-
-=======
 import { useNavigate } from "react-router-dom";
->>>>>>> 634f4c585a5869643c60cb3f77a78e3cbdbfe78b
 
 // eslint-disable-next-line react/prop-types
 const Signin = () => {
