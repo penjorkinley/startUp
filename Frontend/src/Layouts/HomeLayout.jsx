3bec8643dc9715e08e9701563225f0f84999a66c
penjorkinley <penjorkinley77@gmail.com>
2023-06-17 11:21:43 +0600

    nav bar update

--- Frontend/src/Layouts/HomeLayout.jsx
+++ Frontend/src/Layouts/HomeLayout.jsx
@@ -1,15 +1,15 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import { Outlet } from "react-router-dom"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 function HomeLayout() {
   return (
     <div>
-        <Header />
-        <Outlet />
-        <Footer />
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default HomeLayout
+export default HomeLayout;
